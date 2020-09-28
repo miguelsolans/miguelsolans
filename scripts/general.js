7dca8ea311899f21ea9ccda29acd67b17354513c
Miguel Solans <dev.miguelsolans@gmail.com>
2020-09-28 18:48:08 +0100

    Transfered miguelsolans.github.io repository to this very own

---
+++ scripts/general.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    let cursor = true;
+    setInterval(() => {
+        if(cursor) {
+            document.getElementById('cursor').style.opacity = 0;
+            cursor = false;
+        } else {
+            document.getElementById('cursor').style.opacity = 1;
+            cursor = true;
+        }
+    }, 500);
+})
+
+
